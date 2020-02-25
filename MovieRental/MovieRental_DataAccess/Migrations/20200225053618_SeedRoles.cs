ef36655b4bf38c48f1a8565337d8b5c1e5c78ac3
kevin_alvarez93 <omar.alher93@gmail.com>
2020-02-24 23:41:39 -0600

    Add seeders for roles and users

---
+++ MovieRental/MovieRental_DataAccess/Migrations/20200225053618_SeedRoles.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace MovieRental_DataAccess.Migrations
+{
+    public partial class SeedRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+             table: "Roles",
+             columns: new[] { "Name", "Active", "CreatedAt" },
+             values: new object[] { "Admin", true, DateTime.Now }
+         );
+
+            migrationBuilder.InsertData(
+               table: "Roles",
+               columns: new[] { "Name", "Active", "CreatedAt" },
+               values: new object[] { "Client", true, DateTime.Now }
+           );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"TRUNCATE TABLE Roles");
+        }
+    }
+}
---
+++ MovieRental/MovieRental_DataAccess/Migrations/20200225053850_SeedUsers.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MovieRental_DataAccess.Migrations
+{
+    public partial class SeedUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var userSalt = GetSalt();
+            var userPassword = GetHash("k3vin19*" + userSalt);
+            migrationBuilder.InsertData(
+               table: "Users",
+               columns: new[] { "Username", "Password", "PasswordSalt", "RoleId", "Active", "CreatedAt" },
+               values: new object[] { "admin", userPassword, userSalt, 1, true, DateTime.Now }
+           );
+
+            userSalt = GetSalt();
+            userPassword = GetHash("1234" + userSalt);
+            migrationBuilder.InsertData(
+               table: "Users",
+               columns: new[] { "Username", "Password", "PasswordSalt", "RoleId", "Active", "CreatedAt" },
+               values: new object[] { "client", userPassword, userSalt, 2, true, DateTime.Now }
+           );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"TRUNCATE TABLE Users");
+        }
+
+        private static string GetHash(string text)
+        {
+            // SHA512 is disposable by inheritance.  
+            using (var sha256 = SHA256.Create())
+            {
+                // Send a sample text to hash.  
+                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(text));
+                // Get the hashed string.  
+                return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+            }
+        }
+
+        private static string GetSalt()
+        {
+            byte[] bytes = new byte[128 / 8];
+            using (var keyGenerator = RandomNumberGenerator.Create())
+            {
+                keyGenerator.GetBytes(bytes);
+                return BitConverter.ToString(bytes).Replace("-", "").ToLower();
+            }
+        }
+    }
+}
